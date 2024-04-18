ca25146ef72f926353c9d82aff92d31386780d77
Illia_Vorobiov <illia.vorobiov@student.karazin.ua>
2024-04-18 20:21:55 +0200

    Added tests

---
+++ sum_test.py
@@ -0,0 +1,8 @@
+import sum
+
+def test_1():
+    assert sum.sum(1,1) == 2
+def test_2():
+    assert sum.sum(4,-2) == 2
+def test_3():
+    assert sum.sum(1,2) == 3
--- test.py
+++
@@ -1,3 +0,0 @@
-a = input("Enter your number -> ")
-
-print(f'Your number is {a}')
